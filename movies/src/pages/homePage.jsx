3429ea224c38b54d05ca8f7dfa6737ccb7d317b9
Lauren <laurenaoife.mur@gmail.com>
2026-03-24 00:22:17 +0000

    List of movies displayed.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid";
 
 const HomePage = (props) => {
   const movies = props.movies;
-    
+
   return (
-  <Grid container>
+    <Grid container>
       <Grid size={12}>
-          <h1> HomePage </h1>
+        <h1> HomePage </h1>
+      </Grid>
+      <Grid container>
+        <MovieList movies={movies}></MovieList>
       </Grid>
     </Grid>
   );
